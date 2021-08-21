d28cb21e8f03178c35978c245fa13a9f6dd2ac37
sumitsutar <sutar.sumit@outlook.com>
2021-08-21 22:14:01 +0530

    code formatting

--- src/main/java/com/functional/programming/with/javabrains/exercise/Entry.java
+++ src/main/java/com/functional/programming/with/javabrains/exercise/Entry.java
@@ -8,11 +8,11 @@ import java.util.List;
 public class Entry {
     public static void main(String[] args) {
         List<Person> people = Arrays.asList(
-                new Person("Monica","Gellar",25),
-                new Person("Ross","Gellar",25),
-                new Person("Chandler","Bing",25),
-                new Person("Joseph","Tribiani",25),
-                new Person("Phoebe","Buffay",25)
+                new Person("Monica", "Gellar", 25),
+                new Person("Ross", "Gellar", 25),
+                new Person("Chandler", "Bing", 25),
+                new Person("Joseph", "Tribiani", 25),
+                new Person("Phoebe", "Buffay", 25)
         );
         //Step 1 : sort the list by lastname
 
@@ -23,35 +23,32 @@ public class Entry {
         //Step 1 - ANSWER
         Entry entry = new Entry();
         ListManipulator sortListByLastNameLambda = (List<Person> list) -> {
-           Collections.sort(list, new Comparator<Person>() {
+            Collections.sort(list, new Comparator<Person>() {
                 @Override
                 public int compare(Person o1, Person o2) {
                     return o1.getLastName().compareTo(o2.getLastName());
                 }
             });
-           list.forEach(System.out::print);
+            list.forEach(System.out::print);
         };
-        System.out.println("Sorted List by Last Name: \n" );
-        entry.manipulateList(people,sortListByLastNameLambda);
-
+        System.out.println("Sorted List by Last Name: \n");
+        entry.manipulateList(people, sortListByLastNameLambda);
 
 
         //Step 2 - ANSWER
         ListManipulator listPrinterLambda = (List<Person> list) -> list.forEach(System.out::print);
-        System.out.println("\n \n Print All the elements: \n" );
-        entry.manipulateList(people,listPrinterLambda);
+        System.out.println("\n \n Print All the elements: \n");
+        entry.manipulateList(people, listPrinterLambda);
 
         //Step 3 - ANSWER
         ListManipulator filterByLastNameLambda = (List<Person> list) -> {
             (list.stream().filter(s -> s.getLastName().equals("Gellar"))).forEach(System.out::print);
         };
-        System.out.println("\n \n List of Gellars : \n" );
-        entry.manipulateList(people,filterByLastNameLambda);
-
-
+        System.out.println("\n \n List of Gellars : \n");
+        entry.manipulateList(people, filterByLastNameLambda);
     }
 
-    private void manipulateList(List<Person> people,ListManipulator manipulator) {
+    private void manipulateList(List<Person> people, ListManipulator manipulator) {
         manipulator.perform(people);
     }
 }
